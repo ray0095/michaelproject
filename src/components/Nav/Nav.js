d24afb6e3eede8201f6ac7ec891ff23b9a0365d1
Rachel Strong <rachellynnstrong@gmail.com>
2022-02-19 10:42:12 -0500

    adding links to nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,13 +3,16 @@ import './Nav.css';
 
 function Nav() {
     return(
-        <nav class="uk-navbar-container" uk-navbar>
-            <div class="uk-navbar-left">
+        <nav className="uk-navbar-container">
+            <div className="uk-navbar-left">
 
-                <ul class="uk-navbar-nav">
-                    <li class="uk-active"><a href="#">Logo</a></li>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
+                <ul className="uk-navbar-nav">
+                    <li className="uk-active"><a href="/">Logo</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/services">Services</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/reviews">Reviews</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
 
             </div>
