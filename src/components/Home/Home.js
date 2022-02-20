d59e87798a9c5db0503e44fdb0475144bc1cf049
Rachel Strong <rachellynnstrong@gmail.com>
2022-02-20 14:42:37 -0500

    began work on site content and styling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import './Home.css';
 
 function Home() {
-    return(
-        <div>
-            <div className="uk-position-center uk-card uk-card-default uk-card-body uk-animation-fade">
-                    <h1 className="uk-text-center">Welcome</h1>
-            </div>
-        </div>
+    return(    
+    
+            <div>
+                <div className="uk-grid-small uk-grid-match uk-height-match" uk-grid='true'>
+                    <div>
+                        <div className="uk-card uk-card-default uk-card-body uk-position-center-top uk-padding-large">
+                            <h3 className="uk-card-title">From the Midwest to the Coast</h3>
+                            <p>Professional political campaign expertise for every situation</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>  
 
     )
 }
